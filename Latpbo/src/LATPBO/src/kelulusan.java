c39abc5a1ba2de189d206655225df9b24e976c02
Xskyis <nabilafgarezaafgarza@gmail.com>
2022-02-07 18:26:57 +0700

    Achmad Nabil Afgareza_XRPL2_03

---
+++ Latpbo/src/LATPBO/src/kelulusan.java
@@ -0,0 +1,11 @@
+package LATPBO.src;
+public class kelulusan {
+    public static void main(String[] args) throws Exception {
+        siswa ratarataSiswa = new siswa();
+        
+        ratarataSiswa.nilaiproduktif();
+        ratarataSiswa.ratarata();
+
+
+    }
+}
---
+++ Latpbo/src/LATPBO/src/siswa.java
@@ -0,0 +1,24 @@
+package LATPBO.src;
+public class siswa {
+int nilaiproduktif;
+
+
+void ratarata(){
+
+
+    if (nilaiproduktif>=75)
+{
+System.out.println("Keterangan : Selamat Anda LULUS");
+}
+else
+{
+System.out.println("Keterangan : Mohon Maaf Anda Gagal");
+}
+}
+
+void nilaiproduktif(){
+nilaiproduktif = 70;
+System.out.println("Nilai Produktif RPL 2 NABIL adalah : "+nilaiproduktif);
+}
+}
+
