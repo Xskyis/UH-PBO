23867bd16c319eb2475eb4c03ed8a081f31ff0ff
Xskyis <nabilafgarezaafgarza@gmail.com>
2022-02-09 08:23:45 +0700

    first commit

--- Latpbo/src/LATPBO/src/kelulusan.java
+++ Latpbo/src/LATPBO/src/kelulusan.java
@@ -3,6 +3,7 @@ public class kelulusan {
     public static void main(String[] args) throws Exception {
         siswa ratarataSiswa = new siswa();
         
+        ratarataSiswa.biodata();
         ratarataSiswa.nilaiproduktif();
         ratarataSiswa.ratarata();
 
--- Latpbo/src/LATPBO/src/siswa.java
+++ Latpbo/src/LATPBO/src/siswa.java
@@ -1,10 +1,12 @@
 package LATPBO.src;
 public class siswa {
 int nilaiproduktif;
+String nama,kelas;
+int absen;
+
 
 
 void ratarata(){
-
 
     if (nilaiproduktif>=75)
 {
@@ -17,8 +19,24 @@ System.out.println("Keterangan : Mohon Maaf Anda Gagal");
 }
 
 void nilaiproduktif(){
-nilaiproduktif = 70;
-System.out.println("Nilai Produktif RPL 2 NABIL adalah : "+nilaiproduktif);
+nilaiproduktif = 95;
+System.out.println("Nilai Produktif RPL NABIL adalah : "+nilaiproduktif);
+}
+
+void biodata(){
+nama = "Nabil";
+kelas = "XRPL2";
+absen = 3;
+
+System.out.println(" ## Biodata Anda ## ");
+System.out.println();
+System.out.println("Nama  : "+nama);
+System.out.println("Kelas : "+kelas);
+System.out.println("Absen : "+absen);
+System.out.println();
+System.out.println("Nilai Dibawah 75 Tidak Lulus...........");
+System.out.println();
+
 }
 }
 
